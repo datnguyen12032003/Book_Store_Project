3c83eb0a12c6c90b8d259d821d7b4a6a79c7a29c
ThangLPH <thanglphce171637@fpt.edu.vn>
2024-07-14 22:23:00 +0700

    bookdetail css

--- FE/src/components/BookDetailUser.js
+++ FE/src/components/BookDetailUser.js
@@ -203,7 +203,7 @@ const BookDetail = () => {
                         onChange={handleCommentChange}
                     ></textarea>
                    
-                    <button onClick={postComment} className="mt-2 mb-10 bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-o-500 focus:ring-offset-2">
+                    <button onClick={postComment} className="mt-2 mb-10 bg-orange-500 text-white px-4 py-2 hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-o-500 focus:ring-offset-2">
                         Post Comment
                     </button>
                 </div>
